5b8d3a7166cadd8e10932c0650972e40e01f157d
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-01-07 11:40:00 -0500

    Add constant for accessing rest service.

---
+++ eso-api/src/main/java/com/atlas/eso/constant/RestConstants.java
@@ -0,0 +1,8 @@
+package com.atlas.eso.constant;
+
+public final class RestConstants {
+   private RestConstants() {
+   }
+
+   public static final String SERVICE = "eso";
+}
--- eso-base/src/main/java/com/atlas/eso/Server.java
+++ eso-base/src/main/java/com/atlas/eso/Server.java
@@ -2,17 +2,16 @@ package com.atlas.eso;
 
 import java.net.URI;
 
+import com.atlas.eso.constant.RestConstants;
 import com.atlas.shared.rest.RestServerFactory;
-import com.atlas.shared.rest.RestService;
 import com.atlas.shared.rest.UriBuilder;
-import org.glassfish.grizzly.http.server.HttpServer;
 
 import database.PersistenceManager;
 
 public class Server {
    public static void main(String[] args) {
       PersistenceManager.construct("atlas-eso");
-      URI uri = UriBuilder.host(RestService.EQUIPMENT_STORAGE).uri();
+      URI uri = UriBuilder.host(RestConstants.SERVICE).uri();
       RestServerFactory.create(uri, "com.atlas.eso.rest");
    }
 }
